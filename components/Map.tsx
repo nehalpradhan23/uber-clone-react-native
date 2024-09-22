650096ad277f0d97cc453391b6441f2904ded90a
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-09-22 23:22:59 +0530

    show map path

--- components/Map.tsx
+++ components/Map.tsx
@@ -10,6 +10,7 @@ import { Driver, MarkerData } from "@/types/type";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 const Map = () => {
   const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
@@ -84,6 +85,7 @@ const Map = () => {
       showsUserLocation={true}
       userInterfaceStyle="light"
     >
+      {/* markers for drivers ==================================== */}
       {markers.map((marker) => (
         <Marker
           key={marker.id}
@@ -97,6 +99,33 @@ const Map = () => {
           }
         />
       ))}
+      {/* marker for ride path =================== */}
+      {destinationLatitude && destinationLongitude && (
+        <>
+          <Marker
+            key="destination"
+            coordinate={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            title="Destination"
+            image={icons.pin}
+          />
+          <MapViewDirections
+            origin={{
+              latitude: userLatitude,
+              longitude: userLongitude,
+            }}
+            destination={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            apikey={process.env.EXPO_PUBLIC_GOOGLE_API_KEY}
+            strokeColor="#0286ff"
+            strokeWidth={3}
+          />
+        </>
+      )}
     </MapView>
   );
 };
